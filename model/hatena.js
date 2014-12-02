e4a73be713fb68ba183203b50c4c32b63fa7b744
shgtkshruch <shgtk.shruch@gmail.com>
2014-12-02 10:39:19 +0900

    Use native mongodb instead of monk.

--- model/hatena.js
+++ model/hatena.js
@@ -1,37 +1,56 @@
-var monk = require('monk');
-var wrap = require('co-monk');
-var db, bookmarks;
+var thunkify = require('thunkify');
+var MongoClient = require('mongodb').MongoClient;
+var dbName = 'bookmarks';
 
+var connect = thunkify(function (cb) {
+  MongoClient.connect('mongodb://localhost:27017/hatena', function (err, db) { 
+    cb(err, db);
+  });
+});
 
-function connect () {
-  db = monk('localhost/hatena');
-  bookmarks = wrap(db.get('bookmarks'));
-}
+var insert = thunkify(function (db, data, cb) {
+  db.collection(dbName).insert(data, function (err, res) {
+    cb(err, res);
+  });
+});
+
+var find = thunkify(function (db, cb) {
+  db.collection(dbName).find({}).sort({date: -1}).toArray(function (err, docs) {
+    cb(err, docs);
+  });
+});
+
+var remove = thunkify(function (db, cb) {
+  db.collection(dbName).remove({}, function (err, res) {
+    cb(err, res);
+  });
+});
+
+var count = thunkify(function (db, cb) {
+  db.collection(dbName).count(function (err, res) {
+    cb(err, res);
+  });
+});
 
 module.exports = {
-
   save: function *(hbs) {
-    connect();
+    var db = yield connect();
 
-    yield bookmarks.remove({});
+    yield remove(db);
 
-    yield bookmarks.insert(hbs);
-
-    var res = yield bookmarks.find({});
-    console.log(res);
+    yield insert(db, hbs);
 
     db.close();
   },
 
-  find: function *(hbs) {
-    connect();
+  find: function *() {
+    var db = yield connect();
 
-    var res = yield bookmarks.find({});
+    var res = yield find(db);
 
     db.close();
 
     return res;
   }
-
 };
 
