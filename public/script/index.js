1ff31690fc384fe3318df4b5601b9d59c8012cfc
shgtkshruch <shgtk.shruch@gmail.com>
2014-12-03 16:50:05 +0900

    Redesign view.

--- public/script/index.js
+++ public/script/index.js
@@ -5,7 +5,7 @@ var comments = document.querySelector('.cmts');
 // When hb items click, show article description.
 hbs.addEventListener('click', function (e) {
   var el = e.target;
-  if (el.parentNode.classList.contains('hb__thumb')) {
+  if (el.classList.contains('hb__thumb')) {
     suspend(el);
   } else if (el.classList.contains('hb__count')) {
     showComment(el);
@@ -33,21 +33,21 @@ function showDescription (el) {
 
       // Clone node to calculate height.
       var clone = hbBody.cloneNode(true);
+      var wrap = el.querySelector('.hb__wrap');
 
       clone.style.height = 'auto';
-
-      hbs.insertBefore(clone, hbs.lastChild);
+      wrap.insertBefore(clone, wrap.lastChild);
 
       hbBody.style.height = clone.clientHeight + 'px';
       hbBody.classList.add('active');
 
-      hbs.removeChild(clone);
+      wrap.removeChild(clone);
     }
   }
 }
 
 function suspend (el) {
-  var title = el.parentNode.parentNode.nextSibling.textContent;
+  var title = el.parentNode.nextSibling.firstChild.textContent;
   var req = new XMLHttpRequest();
   req.open('GET', '/suspend', true);
   req.setRequestHeader('title', encodeURI(title));
@@ -70,10 +70,19 @@ function loadJS (src) {
   document.body.appendChild(script);
 }
 
+var positonTop;
+
 function showComment (el) {
   var api = 'http://b.hatena.ne.jp/entry/jsonlite/?url=';
-  var url = el.parentNode.nextSibling.href;
+
+  while(!el.classList.contains('hb')) {
+    el = el.parentNode;
+  }
+  positionTop = el.offsetTop;
+
+  var url = el.querySelector('.hb__title').href;
   loadJS(api + encodeURI(url) + '&callback=entryCB');
+
   var wrap = document.querySelector('.wrap');
   if (wrap) {
     comments.removeChild(wrap);
@@ -111,5 +120,6 @@ var entryCB = function(data){
   });
 
   comments.insertBefore(wrap, comments.firstChild);
+  comments.style.top = positionTop + 'px';
 };
 
