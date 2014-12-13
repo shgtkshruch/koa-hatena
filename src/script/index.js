1e52b43fd0d8b541c3e4c041f52b696c0a8d1fa9
shgtkshruch <shgtk.shruch@gmail.com>
2014-12-13 09:28:46 +0900

    Rename suspend.js to remove.js

--- src/script/index.js
+++ src/script/index.js
@@ -5,7 +5,7 @@ var hbs = document.querySelector('.main');
 hbs.addEventListener('click', function (e) {
   var el = e.target;
   if (el.classList.contains('hb__thumb')) {
-    require('./suspend')(el);
+    require('./remove')(el);
   } else if (el.classList.contains('hb__count')) {
     require('./showComments')(el);
   } else {
